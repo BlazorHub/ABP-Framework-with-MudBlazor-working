fec87598f32778a86be6e9a1d3150bd00a300a29
Rhuan Barros <rhuanbarros@gmail.com>
2022-10-31 14:02:45 -0300

    app db schema

--- src/MaterialeShop.Domain/MaterialeShopConsts.cs
+++ src/MaterialeShop.Domain/MaterialeShopConsts.cs
@@ -4,8 +4,8 @@ namespace MaterialeShop;
 
 public static class MaterialeShopConsts
 {
-    public const string DbTablePrefix = "App";
-    public const string DbSchema = null;
+    public const string DbTablePrefix = null;
+    public const string DbSchema = "apps";
     public const string AdminEmailDefaultValue = IdentityDataSeedContributor.AdminEmailDefaultValue;
     public const string AdminPasswordDefaultValue = IdentityDataSeedContributor.AdminPasswordDefaultValue;
 }
