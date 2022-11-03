0936f471067d456e54bc94ace9117d7c94754b03
Rhuan Barros <rhuanbarros@gmail.com>
2022-11-03 16:51:59 -0300

    Listas4Page ordenacao no front ok

--- src/MaterialeShop.Blazor/Pages/Cliente/Listas4Page.razor.cs
+++ src/MaterialeShop.Blazor/Pages/Cliente/Listas4Page.razor.cs
@@ -19,7 +19,23 @@ namespace MaterialeShop.Blazor.Pages.Cliente;
 public partial class Listas4Page
 {
     private IReadOnlyList<ListaWithNavigationPropertiesDto> _listaList { get; set; }
+    private MudTable<ListaWithNavigationPropertiesDto> table;
     private GetListasInput Filter { get; set; }
+
+    public Listas4Page()
+    {
+        // this._logger = logger;
+
+        // NewLista = new ListaCreateDto();
+        // EditingLista = new ListaUpdateDto();
+        Filter = new GetListasInput
+        {
+            MaxResultCount = 10,
+            SkipCount = 0,
+            Sorting = null,
+            FilterText = null
+        };
+    }
 
     protected override async void OnParametersSet()
     {
@@ -28,20 +44,27 @@ public partial class Listas4Page
 
     private async Task GetListasAsync()
     {
-        Filter = new GetListasInput
+        if( table?.RowsPerPage is not null && table?.CurrentPage is not null)
         {
-            MaxResultCount = 50,
-            SkipCount = 0,
-            Sorting = null
-        };
-
-        // Filter.MaxResultCount = 50;
-        // Filter.SkipCount = 0;
-        // Filter.Sorting = null;
+            Filter.MaxResultCount = table.RowsPerPage;
+            Filter.SkipCount = table.RowsPerPage * table.CurrentPage;
+            Filter.Sorting = null;            
+        } else{
+            Filter.MaxResultCount = 10;
+            Filter.SkipCount = 0;
+            Filter.Sorting = null;            
+        }
 
         var result = await ListasAppService.GetListAsync(Filter);
         _listaList = result.Items;
-        // TotalCount = (int)result.TotalCount;
+
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private async void OnSearch(string text)
+    {
+        Filter.FilterText = text;
+        await GetListasAsync();
     }
 
 }
